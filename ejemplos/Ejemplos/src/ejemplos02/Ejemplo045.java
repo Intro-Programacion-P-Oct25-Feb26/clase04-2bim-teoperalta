9c9a15970d610187361028ae4a1a6e054c164c84
teoperalta <teo.peralta.2007@gmail.com>
2026-01-07 15:28:30 -0500

    Ejemplo045

---
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo045.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos02;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo045 {
+
+    public static void main(String[] args) {
+        int[][] arreglo1 = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
+        int[][] arreglo2 = {{1, 2, 3}, {2, 2, 2}, {3, 1, 2}};
+        
+        int[][] arreglo3 = llenarMatriz(arreglo1, arreglo2);
+        double[][] arreglo4 = llenarMatriz2(arreglo1, arreglo2);
+
+        obtenerReporte(arreglo1);
+        obtenerReporte(arreglo2);
+        obtenerReporte(arreglo3);
+        obtenerReporte(arreglo4);
+    }
+
+    public static int[][] llenarMatriz(int[][] a, int[][] b) {
+        int[][] z = new int[3][3]; 
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                z[i][j] = obtenerMultiplicacion(a[i][j], b[i][j]);
+            }
+        }
+        return z;
+    }
+
+    public static double[][] llenarMatriz2(int[][] a, int[][] b) {
+        double[][] z = new double[3][3];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                z[i][j] = obtenerPotencia(a[i][j], b[i][j]);
+            }
+        }
+        return z;
+    }
+
+    
+    public static void obtenerReporte(int[][] matriz) {
+        String cadena = "";
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                cadena = String.format("%s%d\t", cadena, matriz[i][j]);
+            }
+            cadena = String.format("%s\n", cadena);
+        }
+        System.out.println(cadena);
+    }
+
+    // Reporte para matrices double (Sobrecarga)
+    public static void obtenerReporte(double[][] matriz) {
+        String cadena = "";
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                cadena = String.format("%s%.1f\t", cadena, matriz[i][j]);
+            }
+            cadena = String.format("%s\n", cadena);
+        }
+        System.out.println(cadena);
+    }
+
+    public static int obtenerMultiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    public static double obtenerPotencia(int a, int b) {
+        return Math.pow(a, b);
+    }
+}
